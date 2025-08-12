c197df0f18cf8f5192a0cae729e99f0adb19a063
oscar.rs <jeff.cofos@gmail.com>
2025-08-12 17:14:23 +0900

    (fix) customizing ArticleTitle.tsx

--- quartz/components/ArticleTitle.tsx
+++ quartz/components/ArticleTitle.tsx
@@ -3,16 +3,33 @@ import { classNames } from "../util/lang"
 
 const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
   const title = fileData.frontmatter?.title
+  const author = fileData.frontmatter?.author // author 데이터를 가져옵니다.
+
   if (title) {
-    return <h1 class={classNames(displayClass, "article-title")}>{title}</h1>
+    return (
+      <div class={classNames(displayClass, "article-title-container")}>
+        <h1 class={classNames(displayClass, "article-title")}>{title}</h1>
+        {author && <p class="article-author">by {author}</p>} {/* author가 있으면 표시합니다. */}
+      </div>
+    )
   } else {
     return null
   }
 }
 
 ArticleTitle.css = `
-.article-title {
+.article-title-container {
   margin: 2rem 0 0 0;
+}
+
+.article-title {
+  margin: 0;
+}
+
+.article-author {
+  margin: 0.5rem 0 0 0;
+  font-style: italic;
+  color: var(--darkgray);
 }
 `
 
